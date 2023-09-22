62e257b9ff9c06867ed6a90d3aee1aba08df3aba
nvhao96 <nvhao182@gmail.com>
2023-09-22 21:21:14 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const contactsRouter = require("./app/routes/contact.route");
 
 const app = express();
 
@@ -8,8 +9,11 @@ app.use(cors());
 app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 
+
 app.get("/", (req, res) => {
     res.json({ messeage: "Welcome to contact book application"});
 });
+
+app.use("/api/contacts", contactsRouter);
 
 module.exports = app;
---
+++ app/controllers/contact.controller.js
@@ -0,0 +1,34 @@
+exports.create = ( req, res ) =>
+{
+    res.send( { message: "Create handler"} );
+};
+
+exports.findAll = ( req, res ) =>
+{ 
+    res.send({ message: "FindAll handler" });
+};
+
+exports.findOne = ( req, res ) =>
+{ 
+    res.send({ message : "FindOne handler" });
+};
+
+exports.update = ( req, res ) =>
+{ 
+    res.send({ message : "Update handler" });
+};
+
+exports.delete = ( req, res ) =>
+{ 
+    res.send({ message : "Delete handler" });
+};
+
+exports.deleteAll = ( req, res ) =>
+{ 
+    res.send({ message : "DeleteAll handler" });
+};
+
+exports.findAllFavorite = ( req, res ) =>
+{ 
+    res.send( { message: "FindAll favorite handler" } );
+};
